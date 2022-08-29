b3e8029e60ee48d95fcb0484e702c5c057bcd1f4
ngovanminh3006 <mch300621@gmail.com>
2022-08-29 11:18:49 +0700

    add cart and help screen

---
+++ Appdev/Controllers/Help.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace AppDev.Controllers
+{
+    public class Help : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
