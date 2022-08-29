50a02b89584f1d823698c6e99a113ae0a674775e
ngovanminh3006 <mch300621@gmail.com>
2022-08-29 14:24:12 +0700

    Add Search Input into Books Index View

---
+++ Appdev/ViewModels/BookIndexViewModel.cs
@@ -0,0 +1,11 @@
+﻿using AppDev.Models;
+
+namespace AppDev.ViewModels
+{
+    public class BookIndexViewModel
+    {
+        public ICollection<Book> Books { get; set; } = null!;
+
+        public SearchViewModel SearchViewModel { get; set; } = new();
+    }
+}
